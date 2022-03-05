97f98ceb0780748ee31abd7abf612d66c6f9ebce
Brian Sokoloski <brian.sokoloski@gmail.com>
2022-03-05 15:05:26 -0600

    initial spring boot java web app

---
+++ src/test/java/com/sokoloski/airquality/AirqualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sokoloski.airquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirqualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
